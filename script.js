14537ce60e8a90eba4480626c74bb9ee70e6f29b
janoslim <janoslim@users.noreply.github.com>
2026-06-09 08:19:48 +0000

    adopt sweetbbak visual template and map to personal content

--- script.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const years = document.querySelectorAll("#year");
-  if (years.length) {
-    years.forEach((el) => {
-      el.textContent = String(new Date().getFullYear());
-    });
-  }
-})();
